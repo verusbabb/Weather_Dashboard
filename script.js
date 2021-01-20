8b764be2bba33effc5bd7da310c6fd959d784f9b
Steve Babb <steve.babb@outlook.com>
2021-01-19 18:19:30 -0600

    less html, better js

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
+
 // Accessing moment.js to capture dates
 var now = moment().format('MM/D/YY'); //for calendar date at the top of page
-var whichHour = moment().format("H"); //for checking past, present, future of each hour-block in work-planner
-var exactTime = moment().format("h:mm:ss")
-var tomorrow = moment().add(1, 'days').format('MM/D/YY');
+var tomorrow0 = moment().add(1, 'days').format('MM/D/YY');
 var tomorrow1 = moment().add(2, 'days').format('MM/D/YY');
 var tomorrow2 = moment().add(3, 'days').format('MM/D/YY');
 var tomorrow3 = moment().add(4, 'days').format('MM/D/YY');
 var tomorrow4 = moment().add(5, 'days').format('MM/D/YY');
+var dayName;
 
-console.log(tomorrow, tomorrow1, tomorrow2, tomorrow3, tomorrow4);
+$("button").on("click", function(event) {
+  event.preventDefault();
+  
+  // assigning the input text to variable city
+  var city = $("#search").val().trim();
+  
 
-// added this, but not using; purpose was to be a trigger value for resetting calendar items at midnight
-// not practical unless data is being stored on an always running server (so here for exercise, not functionality)
-// see notes on commented-out code block at bottom of script
-var midNight = "00:00:00";
-
-// Assigning moment.js date to top of calendar
-var $todaysDate = $("#currentDay");
-$todaysDate.text(now);
-console.log(now);
-
+console.log(city);
+  
+  // saving the tast description to local storage with "hour" as the key and "task" as value
+  localStorage.setItem("location", JSON.stringify(city));
+});
 
 // This is the API key, adding for getting measurements in imperial units.
 var APIKey = "f21ee75183114c7c096d92749641d1f4&units=imperial";
 
-// Here we are building the URL we need to query the database
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-//   "q=Lawrence,Kansas&appid=" + APIKey;
-
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=lawrence&appid=" + APIKey;
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=atlanta&appid=" + APIKey;
 
 // Here we run our AJAX call to the OpenWeatherMap API
 $.ajax({
@@ -36,27 +32,40 @@ $.ajax({
   method: "GET"
 })
   // We store all of the retrieved data inside of an object called "response"
-  .then(function(response) {
+  .then(function (response) {
 
     // Log the queryURL
     console.log(queryURL);
 
     // Log the resulting object
     console.log(response);
-    
-    // Create today's cityStats list
-    $("#mainScreen").append("<span class='card-title'>" + response.city.name + " weather details");
-    var cityStats = $("<ul>");
-    // var stat = $("<li>");
+
+
+
+    // Create and display current weather stats
+    $("#mainScreen").append("<span class='card-title'>" + response.city.name + " current weather:");
+    var cityStats = $("<ul>").append(temp, wind, humidity);
     $("#mainScreen").append(cityStats);
-    $(cityStats).after().append("<li>" + "Current temperature: " + response.list[0].main.temp.toFixed(0) + "° fahrenheit");
-    $(cityStats).after().append("<li>" + "Humidity: " + response.list[0].main.humidity + "%");
-    $(cityStats).after().append("<li>" + "Wind: " + response.list[0].wind.speed.toFixed(0) + " mph");
+    var temp = $("<li>").append("Current temperature: " + response.list[0].main.temp.toFixed(0) + "° fahrenheit");
+    var wind = $("<li>").append("Wind: " + response.list[0].wind.speed.toFixed(0) + " mph");
+    var humidity = $("<li>").append("Humidity: " + response.list[0].main.humidity + "%");
+    cityStats.append(temp, wind, humidity);
+    
 
+    //Create and display 5 day forecast data
+    var cityStats0 = $("<ul>").append(date0, icon, temp0, wind0, humidity0);
+    $("card").append(cityStats0);
+    var date0 = $("<h5>").append(tomorrow0);
+    var icon = $('<img src="http://openweathermap.org/img/wn/04n@2x.png"/>');
+    var temp0 = $("<li>").append("Temp: " + response.list[0].main.temp.toFixed(0) + "°");
+    var wind0 = $("<li>").append("Wind: " + response.list[0].wind.speed.toFixed(0) + " mph");
+    var humidity0 = $("<li>").append("Humidity: " + response.list[0].main.humidity + "%");
+    cityStats0.append(date0, icon, temp0, wind0, humidity0);
     
     
+    console.log(response.list[0].weather[0].icon);
+    
+
   });
-
-
 
 
